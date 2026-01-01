88154262a4b0d1c62149a72aab844b2fb3a0e12a
mikeybeezy <mikeybabs40@gmail.com>
2026-01-01 23:29:46 +0000

    feat: bootstrap apps via kubernetes_manifest

--- modules/kubernetes_addons/main.tf
+++ modules/kubernetes_addons/main.tf
@@ -18,3 +18,13 @@ resource "helm_release" "argocd" {
 
   # Add common tags to the deployment if supported by the chart, or just track via Terraform state.
 }
+
+resource "kubernetes_manifest" "applications" {
+  for_each = var.path_to_app_manifests != "" ? fileset(var.path_to_app_manifests, "*.yaml") : []
+
+  manifest = yamldecode(file("${var.path_to_app_manifests}/${each.value}"))
+
+  depends_on = [
+    helm_release.argocd
+  ]
+}
--- modules/kubernetes_addons/variables.tf
+++ modules/kubernetes_addons/variables.tf
@@ -4,6 +4,12 @@ variable "argocd_version" {
   default     = "5.46.7" # Pinned stable version
 }
 
+variable "path_to_app_manifests" {
+  description = "Path to the directory containing ArgoCD Application manifests (YAMLs) to verify/bootstrap."
+  type        = string
+  default     = ""
+}
+
 variable "tags" {
   description = "A map of tags to add to all resources."
   type        = map(string)
